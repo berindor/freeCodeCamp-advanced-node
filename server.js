749d93000dd6a5a6660003e58f05aa6876efffd4
Beringer Dorottya <beringerdorottya@gmail.com>
2024-02-27 15:37:53 +0100

    fcc advancednode task 20

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const app = express();
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -20,8 +25,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
+    key: 'express.sid',
     resave: true,
     saveUninitialized: true,
+    store: store,
     cookie: { secure: false }
   })
 );
@@ -41,12 +48,34 @@ myDB(async client => {
   });
 });
 
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
   let currentUsers = 0;
   io.on('connection', socket => {
-    console.log('A user has connected');
+    console.log('user ' + socket.request.user.username + ' connected');
     ++currentUsers;
     io.emit('user count', currentUsers);
     socket.on('disconnect', () => {
