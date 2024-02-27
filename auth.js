35bad2e22429c33bc31fe29d693f44b923d14d02
Beringer Dorottya <beringerdorottya@gmail.com>
2024-02-27 11:31:40 +0100

    fcc advancednode task 15

--- auth.js
+++ auth.js
@@ -2,6 +2,8 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const bcrypt = require('bcrypt');
 const { ObjectID } = require('mongodb');
+const GitHubStrategy = require('passport-github');
+require('dotenv').config();
 
 module.exports = function (app, myDataBase) {
   passport.serializeUser((user, done) => {
@@ -23,5 +25,20 @@ module.exports = function (app, myDataBase) {
         return done(null, user);
       });
     })
+  );
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: 'https://berindor-fcc-advancednode.onrender.com/auth/github/callback'
+      },
+      function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        //        myDataBase.findOrCreate({ githubId: profile.id }, function (err, user) {
+        //          return cb(err, user);
+        //        });
+      }
+    )
   );
 };
